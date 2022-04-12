663b7246ba0c958f52f4762b4f922a710199aa71
Windows81 <nastyahmede@gmail.com>
2022-04-12 23:26:00 +0000

    Fixed Chipotle code structure?

--- _07603178367.lua
+++ _07603178367.lua
@@ -12,22 +12,4 @@ for _, g in next, game:GetDescendants() do
 	if g:isA 'GuiBase2d' then g.AutoLocalize = false end
 end
 
---[[
-_G.cmg_t = t
-game.ReplicatedStorage.RemoteEvent:FireServer('UpdateReturningPlayer')
-wait(.5)
-game.ReplicatedStorage.RemoteEvent:FireServer('startBurritoBuilder')
-wait(.5)
-game.ReplicatedStorage.RemoteFunction:InvokeServer('getBurritoBuilderData')
-wait(.5)
-game.ReplicatedStorage.RemoteEvent:FireServer('togglePlayerVisible', true)
-
-while _G.cmg_t == t do
-	wait(d)
-	c = c + 1
-	print(c)
-end
-game.ReplicatedStorage.RemoteEvent:FireServer('burritoBuilderResults', {c})
-wait(.5)
-game.ReplicatedStorage.RemoteEvent:FireServer('togglePlayerVisible', false)
-]]
+exec'PLACE'
