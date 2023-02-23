7bbc26da2abad6c6a5442aec7a7ddda6ccc6f071
Windows81 <nastyahmede@gmail.com>
2023-02-23 21:36:04 +0000

    2023-02-23

---
+++ set-country.lua
@@ -0,0 +1,31 @@
+--[==[HELP]==
+Overrides the local player's for future calls to GetCountryRegionForPlayerAsync.
+
+[1] - string | nil
+	The two-letter ISO country code which shall be returned.
+	Defaults to 'GB'.
+]==] --
+--
+local args = _E and _E.ARGS or {}
+local CHOSEN_REGION = args[1] or 'GB'
+
+local ls = game:GetService 'LocalizationService'
+local lp = game.Players.LocalPlayer
+
+local hook_m
+hook_m = hookmetamethod(
+	ls, '__namecall', function(self, ...)
+		local m_name = getnamecallmethod()
+		local m_args = {...}
+
+		if self ~= ls or m_name ~= 'GetCountryRegionForPlayerAsync' then --
+			return hook_m(self, ...)
+		end
+
+		local plr = m_args[1]
+		if plr ~= lp then --
+			return hook_m(self, ...)
+		end
+
+		return CHOSEN_REGION
+	end)
